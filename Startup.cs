5f04a950ee9ee5a970859f4606ecc4fdad3f61f1
cuongdv18020vtca <cuongdv.nde18020@vtc.edu.vn>
2019-11-18 09:37:09 +0700

    dsfsf

--- Startup.cs
+++ Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -13,47 +14,65 @@ using Vtc_Freelancer.Services;
 
 namespace Vtc_Freelancer
 {
-    public class Startup
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddScoped<MyDbContext, MyDbContext>();
-            services.AddScoped<HashPassword, HashPassword>();
-            services.AddScoped<UserService, UserService>();
-            services.AddControllersWithViews();
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-            app.UseRouting();
-            app.UseAuthorization();
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
-            });
-        }
+      Configuration = configuration;
     }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddScoped<MyDbContext, MyDbContext>();
+      services.AddScoped<HashPassword, HashPassword>();
+      services.AddScoped<UserService, UserService>();
+      services.AddControllersWithViews();
+      services.AddSession(options =>
+{
+  // Set a short timeout for easy testing.
+  options.IdleTimeout = TimeSpan.FromHours(3);
+  options.Cookie.HttpOnly = true;
+  // Make the session cookie essential
+  options.Cookie.IsEssential = true;
+});
+      services.Configure<CookiePolicyOptions>(options =>
+            {
+        // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+        options.CheckConsentNeeded = context => true;
+              options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+      services.AddScoped<MyDbContext, MyDbContext>();
+
+    }
+
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+      else
+      {
+        app.UseExceptionHandler("/Home/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+      }
+      app.UseHttpsRedirection();
+      app.UseStaticFiles();
+      app.UseRouting();
+      app.UseAuthorization();
+      app.UseSession();
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapControllerRoute(
+                  name: "default",
+                  pattern: "{controller=Home}/{action=Index}/{id?}");
+      });
+    }
+  }
 }
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,14 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function Checkpass() {
+  var a = document.getElementById("p1").value;
+  var b = document.getElementById("p2").value;
+  if (a !== b) {
+    document.getElementById("error").innerHTML = "Invalid password";
+    document.getElementById("but").disabled = true;
+  } else {
+    document.getElementById("error").innerHTML = "Valid Password";
+    document.getElementById("but").disabled = false;
+  }
+}
