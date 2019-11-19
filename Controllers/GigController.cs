9046aa97953f6f8a9a952a3facb1fdf989d688f2
boydungbg <dunglc.nde18055@vtc.edu.vn>
2019-11-19 13:12:53 +0700

    ban giao buoc 2 add package

--- Controllers/GigController.cs
+++ Controllers/GigController.cs
@@ -3,59 +3,52 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using Vtc_Freelancer.ActionFilter;
 using Vtc_Freelancer.Models;
 using Vtc_Freelancer.Services;
-using Vtc_Freelancer.ActionFilter;
 
-namespace Vtc_Freelancer.Controllers
-{
-    [Authentication]
-    public class GigController : Controller
-    {
+namespace Vtc_Freelancer.Controllers {
+    // [Authentication]
+    public class GigController : Controller {
         private GigService gigService;
-        public GigController(GigService gigService)
-        {
+        public GigController (GigService gigService) {
             this.gigService = gigService;
         }
-        [HttpGet("/CreateService")]
-        public IActionResult CreateService()
-        {
-            return View();
+
+        [HttpGet ("/CreateService")]
+        public IActionResult CreateService () {
+            return View ();
         }
-        [HttpPost("/CreateServiceStep1")]
-        public IActionResult CreateServiceStep1(string title, string category, string subcategory, string tags)
-        {
-            int ServiceId = gigService.CreateServiceStepOne(title, category, subcategory, tags);
-            if (ServiceId == 0)
-            {
-                return Redirect("/CreateService");
+
+        [HttpPost ("/CreateServiceStep1")]
+        public IActionResult CreateServiceStep1 (string title, string category, string subcategory, string tags) {
+            int ServiceId = gigService.CreateServiceStepOne (title, category, subcategory, tags);
+            if (ServiceId == 0) {
+                return Redirect ("/CreateService");
             }
-            HttpContext.Session.SetInt32("serviceId", ServiceId);
-            return Redirect("/CreateService");
+            HttpContext.Session.SetInt32 ("serviceId", ServiceId);
+            return Redirect ("/CreateService");
         }
-        // [HttpPost("/CreateServiceStep2")]
-        // public IActionResult CreateServiceStep2(string title, string category, string subcategory, string tags)
-        // {
-        //     int ServiceId = gigService.CreateServiceStepOne(title, category, subcategory, tags);
-        //     if (ServiceId == 0)
-        //     {
-        //         return Redirect("/CreateService");
-        //     }
-        //     // HttpContext.ISession.SetInt32("ServiceId", ServiceId);
-        //     return Redirect("/CreateService");
-        // }
+
+        [HttpPost ("/CreateServiceStep2")]
+        public IActionResult CreateServiceStep2 (Package pagkage) {
+            System.Console.WriteLine(pagkage.Name);
+            int? ServiceId = HttpContext.Session.GetInt32 ("serviceId");
+            if (gigService.GetServiceByID (ServiceId) != null) {
+                bool Check = gigService.CreateServiceStepTwo (pagkage, ServiceId);
+                return Redirect ("/CreateService");
+            }
+            return Redirect ("/CreateService");
+        }
+
         [HttpPost]
-        public IActionResult reportGig(int UserId, int ServiceId, string titleReport, string contentReport)
-        {
-            if (gigService.reportGig(UserId, ServiceId, titleReport, contentReport))
-            {
+        public IActionResult reportGig (int UserId, int ServiceId, string titleReport, string contentReport) {
+            if (gigService.reportGig (UserId, ServiceId, titleReport, contentReport)) {
                 ViewBag.ReportStatus = true;
-            }
-            else
-            {
+            } else {
                 ViewBag.ReportStatus = false;
             }
-            return View();
+            return View ();
         }
     }
 }
--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,81 +1,69 @@
+using System;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Vtc_Freelancer.Models;
 using Vtc_Freelancer.Services;
-using System;
-namespace Vtc_Freelancer.Controllers
-{
-    public class UserController : Controller
-    {
+namespace Vtc_Freelancer.Controllers {
+    public class UserController : Controller {
         private UserService userService;
-        public UserController(UserService userService)
-        {
+        public UserController (UserService userService) {
             this.userService = userService;
         }
-        [HttpPost("/Register")]
-        public IActionResult Register(string username, string email, string password)
-        {
-            if (userService.Register(username, email, password))
-            {
+
+        [HttpPost ("/Register")]
+        public IActionResult Register (string username, string email, string password) {
+            if (userService.Register (username, email, password)) {
                 ViewBag.Noti = true;
             }
-            return Redirect("/Login");
+            return Redirect ("/Login");
         }
-        [HttpGet("/Register")]
-        public IActionResult Register()
-        {
-            return View();
-        }
-        [HttpPost("/Login")]
 
-        public IActionResult Login(string email, string password)
-        {
-            Users user = new Users();
-            user = userService.Login(email, password);
-            if (user == null)
-            {
-                return Redirect("/Login");
-            }
-            else
-            {
-                HttpContext.Session.SetInt32("UserId", user.UserId);
+        [HttpGet ("/Register")]
+        public IActionResult Register () {
+            return View ();
+        }
+
+        [HttpPost ("/Login")]
+
+        public IActionResult Login (string email, string password) {
+            Users user = new Users ();
+            user = userService.Login (email, password);
+            if (user == null) {
+                return Redirect ("/Login");
+            } else {
+                HttpContext.Session.SetInt32 ("UserId", user.UserId);
                 ViewBag.Notification = true;
-                if (user.Email == "admin@gmail.com")
-                {
-                    return Redirect("/Admin/Dashboard");
-                }
-                else
-                {
-                    return Redirect("/");
+                if (user.Email == "admin@gmail.com") {
+                    return Redirect ("/Admin/Dashboard");
+                } else {
+                    return Redirect ("/");
                 }
             }
         }
-        [HttpGet("/Login")]
-        public IActionResult Login()
-        {
-            return View();
+
+        [HttpGet ("/Login")]
+        public IActionResult Login () {
+            return View ();
         }
-        public IActionResult Logout()
-        {
-            HttpContext.Session.Clear();
-            return Redirect("/");
+        public IActionResult Logout () {
+            HttpContext.Session.Clear ();
+            return Redirect ("/");
         }
-        [HttpPost("/EditProfile")]
-        public IActionResult EditProfile(int Id, string UserName, string Email)
-        {
-            bool Edit = userService.EditProfile(Id, UserName, Email);
-            if (Edit == true)
-            {
-                return Redirect("/");
+
+        [HttpPost ("/EditProfile")]
+        public IActionResult EditProfile (int Id, string UserName, string Email) {
+            bool Edit = userService.EditProfile (Id, UserName, Email);
+            if (Edit == true) {
+                return Redirect ("/");
             }
-            return View();
+            return View ();
         }
-        [HttpGet("/EditProfile")]
-        public IActionResult EditProfile()
-        {
-            return View();
+
+        [HttpGet ("/EditProfile")]
+        public IActionResult EditProfile () {
+            return View ();
         }
     }
 }
