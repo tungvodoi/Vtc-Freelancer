b266a8bafcfe9a2343a36db1aa0a377fff9fd8c9
doanngocthach <thachdn.nde18048@vtc.edu.vn>
2019-12-03 22:27:33 +0700

    thach commit 03/12/2019

--- Controllers/SellerController.cs
+++ Controllers/SellerController.cs
@@ -1,27 +1,59 @@
 
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Vtc_Freelancer.Models;
 using Vtc_Freelancer.Services;
 using Microsoft.AspNetCore.Http;
+using Vtc_Freelancer.ActionFilter;
+using System;
 
 namespace Vtc_Freelancer.Controllers
 {
-  // [Authentication]
-  public class SellerController : Controller
-  {
-    private UserService userService;
-    private readonly ILogger<HomeController> _logger;
-
-    public SellerController(ILogger<HomeController> logger, UserService userService)
+    // [Authentication]
+    public class SellerController : Controller
     {
-      this.userService = userService;
-      _logger = logger;
-    }
+        private UserService userService;
+        private GigService gigService;
+        private readonly ILogger<HomeController> _logger;
 
-    public IActionResult Index()
-    {
-      ViewBag.UserName = HttpContext.Session.GetString("UserName");
-      return View();
+        public SellerController(ILogger<HomeController> logger, UserService userService, GigService gigService)
+        {
+            this.userService = userService;
+            this.gigService = gigService;
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.UserName = HttpContext.Session.GetString("UserName");
+            return View();
+        }
+        // [HttpGet("/fdasfaf")]
+        [Route("{username}")]
+        [HttpGet]
+        public IActionResult ProfileSeller(string username)
+        {
+            if (username == null)
+            {
+                return Redirect("/");
+            }
+            Users users = userService.GetUserByUsername(username);
+            if (users == null)
+            {
+                return Redirect("/");
+            }
+
+            Seller seller = userService.GetSellerByUserID(users.UserId);
+            List<Service> services = new List<Service>();
+            services = gigService.GetServicesBySellerId(seller.SellerId);
+            if (seller == null)
+            {
+                return Redirect("/");
+            }
+            ViewBag.userProfile = users;
+            ViewBag.listServiceProfile = services;
+            return View();
+        }
     }
-  }
 }
