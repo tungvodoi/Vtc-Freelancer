2af38969fafc0cbccdc9f1bdeec960c6c6108de0
doanngocthach <thachdn.nde18048@vtc.edu.vn>
2020-02-26 10:55:21 +0700

    Updates

---
+++ precache-manifest.1c3f9e11b4ce87e7b21ca5d9cda3326a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8f75f04b475fb8de532cd9371023e61",
+    "url": "/vaicalon/index.html"
+  },
+  {
+    "revision": "353211b9025802711697",
+    "url": "/vaicalon/static/css/main.669c821e.chunk.css"
+  },
+  {
+    "revision": "aa33c2d951243e751007",
+    "url": "/vaicalon/static/js/2.64626e8e.chunk.js"
+  },
+  {
+    "revision": "d9b445fd12d8bcaca03d304ff543ec4e",
+    "url": "/vaicalon/static/js/2.64626e8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "353211b9025802711697",
+    "url": "/vaicalon/static/js/main.fc5c340c.chunk.js"
+  },
+  {
+    "revision": "1724ffc73ba4f28a790d",
+    "url": "/vaicalon/static/js/runtime-main.44548b68.js"
+  }
+]);
